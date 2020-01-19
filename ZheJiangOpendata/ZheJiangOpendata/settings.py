8a2db3071927e22abd1fdf4bd7f233df781d2630
yuangongping <1030617785@qq.com>
2020-01-19 11:53:30 +0800

    浙江省数据开发平台数据集模块采集

--- ZheJiangOpendata/ZheJiangOpendata/settings.py
+++ ZheJiangOpendata/ZheJiangOpendata/settings.py
@@ -1,42 +1,94 @@
 # -*- coding: utf-8 -*-
-import socket
-# 工程名
+
+# Scrapy settings for GuiYangOpendata project
+#
+# For simplicity, this file contains only settings considered important or
+# commonly used. You can find more settings consulting the documentation:
+#
+#     https://doc.scrapy.org/en/latest/topics/settings.html
+#     https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#     https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+
 BOT_NAME = 'ZheJiangOpendata'
+
 SPIDER_MODULES = ['ZheJiangOpendata.spiders']
 NEWSPIDER_MODULE = 'ZheJiangOpendata.spiders'
 
-# 爬虫机器协议，即那些内容可爬取， 那些不可以
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'GuiYangOpendata (+http://www.yourdomain.com)'
+
+# Obey robots.txt rules
 ROBOTSTXT_OBEY = False
-# 设置日志
-# LOG_LEVEL = 'INFO'
-# 爬虫的并行请求量， 默认16
+
+# Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 8
-# 爬虫的下载延迟
-DOWNLOAD_DELAY = 0.5
-# spider中间件
-SPIDER_MIDDLEWARES = {
-   'ZheJiangOpendata.middlewares.ZheJiangOpendataSpiderMiddleware': 543,
-}
-# 下载中间件
-DOWNLOADER_MIDDLEWARES = {
-  'ZheJiangOpendata.middlewares.ZheJiangOpendataDownloaderMiddleware': 543,
-}
-# item 与 pipelines启动设置
+
+# Configure a delay for requests for the same website (default: 0)
+# See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
+# See also autothrottle settings and docs
+DOWNLOAD_DELAY = 2
+# The download delay setting will honor only one of:
+#CONCURRENT_REQUESTS_PER_DOMAIN = 16
+#CONCURRENT_REQUESTS_PER_IP = 16
+
+# Disable cookies (enabled by default)
+#COOKIES_ENABLED = False
+
+# Disable Telnet Console (enabled by default)
+#TELNETCONSOLE_ENABLED = False
+
+# Override the default request headers:
+#DEFAULT_REQUEST_HEADERS = {
+#   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+#   'Accept-Language': 'en',
+#}
+
+# Enable or disable spider middlewares
+# See https://doc.scrapy.org/en/latest/topics/spider-middleware.html
+#SPIDER_MIDDLEWARES = {
+#    'ZheJiangOpendata.middlewares.GuiyangopendataSpiderMiddleware': 543,
+#}
+
+# Enable or disable downloader middlewares
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html
+#DOWNLOADER_MIDDLEWARES = {
+#    'ZheJiangOpendata.middlewares.GuiyangopendataDownloaderMiddleware': 543,
+#}
+
+# Enable or disable extensions
+# See https://doc.scrapy.org/en/latest/topics/extensions.html
+#EXTENSIONS = {
+#    'scrapy.extensions.telnet.TelnetConsole': None,
+#}
+
+# Configure item pipelines
+# See https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 ITEM_PIPELINES = {
-   'ZheJiangOpendata.pipelines.ZheJiangOpendataPipeline': 300,
+   'ZheJiangOpendata.pipelines.DatasetDownloadPipeline': 300,
 }
-# 自定义数据采集调度平台数据采集记录系统接口地址
-DATARECORDADDRESS = 'http://172.16.119.6:5000/add_record'
 
-# 非结构化数据（文件、视频、音频、图像 4类）存储接口地址
-UNSTRUCTUREDDATASTORAGEADDRESS = 'http://172.16.119.3:1213/savefile'
+FILES_STORE = 'files'
+FILES_EXPIRES = 90
 
-# 数据采集类型（STRUCTURED：仅结构化， UNSTRUCTURED: 仅（视频、图像、音频、文件）等非结构化；BOTH二者都有）
-COLLECTDATATYPE = 'STRUCTURED'
+# Enable and configure the AutoThrottle extension (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/autothrottle.html
+#AUTOTHROTTLE_ENABLED = True
+# The initial download delay
+#AUTOTHROTTLE_START_DELAY = 5
+# The maximum download delay to be set in case of high latencies
+#AUTOTHROTTLE_MAX_DELAY = 60
+# The average number of requests Scrapy should be sending in parallel to
+# each remote server
+#AUTOTHROTTLE_TARGET_CONCURRENCY = 1.0
+# Enable showing throttling stats for every response received:
+#AUTOTHROTTLE_DEBUG = False
 
-# 代码所处阶段，分为开发环境（development）与生产环境(production)两个阶段，开发环境表示调试阶段， 生产环境表示正常运行阶段
-hostname = socket.gethostname()  # 获取计算机名称
-# 获取本机IP
-ip = socket.gethostbyname(hostname)
-# 如果代码是在服务器上运行， 则连接的是服务器的地址， 否则连接的是自己本地数据库的地址
-ENVIRINMENT = 'production' if '172.16.119' in ip else 'development'
+# Enable and configure HTTP caching (disabled by default)
+# See https://doc.scrapy.org/en/latest/topics/downloader-middleware.html#httpcache-middleware-settings
+#HTTPCACHE_ENABLED = True
+#HTTPCACHE_EXPIRATION_SECS = 0
+#HTTPCACHE_DIR = 'httpcache'
+#HTTPCACHE_IGNORE_HTTP_CODES = []
+#HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
+
